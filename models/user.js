1829f75afbd048d5c9e20332a75f0f50057b9767
ssss-1b <suzanne.biju@ontariotechu.net>
2024-12-05 23:47:47 -0500

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,46 +1,48 @@
-//User Model
-let mongoose = require ('mongoose');
+// User Model
+let mongoose = require('mongoose');
 let passportLocalMongoose = require('passport-local-mongoose');
 
-let User = mongoose.Schema({
-    username:
-    {
-        type:String,
-        default:"",
-        trim:true,
-        required:'Username is required to login'
+let userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        default: "",
+        trim: true,
+        required: 'Username is required to login'
     },
-    /*FOR NOW, password is commented(will come back to fix)
-    password:
-    {
-        type:String,
-        default:"",
-        trim:true,
-        required:'Password is required to login'
-    }, */
+    /* FOR NOW, password is commented (will come back to fix)*/
+    password: {
+        type: String,
+        default: "",
+        trim: true,
+        required: 'Password is required to login'
+    }, 
 
-    displayName:
-    {
-        type:String,
-        default:"",
-        trim:true,
-        required:'Display Name is required to login'
+    displayName: {
+        type: String,
+        default: "",
+        trim: true,
+        required: 'Display Name is required to login'
     },
-    created:{
-        type:Date,
+    created: {
+        type: Date,
         default: Date.now
     },
-    update:{
-        type:Date,
+    update: {
+        type: Date,
         default: Date.now
     }
-},
-{
+}, {
     collections: "user"
-}
-)
+});
 
-//Options for User model
-let options = ({MissingPasswordError:'Your Password is Incorrect/Missing'});
-User.plugin(passportLocalMongoose,options);
-module.exports.User = mongoose.model('User', User);
+// Options for User model
+let options = {
+    errorMessages: {
+        MissingPasswordError: 'Your Password is Incorrect/Missing'
+    }
+};
+
+userSchema.plugin(passportLocalMongoose, options);
+
+// Create and export the model
+module.exports.User = mongoose.model('User', userSchema); 
