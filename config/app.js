5eb0e7822c3d161644818d1836f72d34a40f3f66
ssss-1b <suzanne.biju@ontariotechu.net>
2024-12-05 23:46:32 -0500

    Update app.js

--- config/app.js
+++ config/app.js
@@ -1,19 +1,13 @@
-// In your app.js file
-
+let createError = require('http-errors');
 let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
+let session = require('express-session');
+let passport = require('passport');
+let passportlocal = require('passport-local');
+let flash = require('connect-flash');
 let mongoose = require('mongoose');
-let createError = require('http-errors');
-
-// Include your routes
-let landingRouter = require('../routes/landing');
-let usersRouter = require('../routes/users');
-let publicRouter = require('../routes/public'); 
-let ticketRouter = require('../routes/ticket');
-let loginRouter = require('../routes/login'); // Make sure this is included
-
 let app = express();
 
 // MongoDB connection
@@ -22,7 +16,36 @@ mongoose.connect(DB.URI);
 let mongoDB = mongoose.connection;
 mongoDB.on('error', console.error.bind(console, 'Connection Error: '));
 mongoDB.once('open', () => {
-  console.log('connected with MongoDB');
+  console.log('Connected to MongoDB');
+});
+
+// User model and Passport authentication
+let userModel = require('../models/user');
+let User = userModel.User;
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Express-session setup
+app.use(session({
+  secret: "SomeSecret",
+  saveUninitialized: false,
+  resave: false
+}));
+
+// Passport initialization
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Flash messages
+app.use(flash());
+
+// Debugging middleware
+app.use((req, res, next) => {
+  console.log(`Request URL: ${req.url}`);
+  res.locals.loggedIn = req.isAuthenticated();
+  next();
 });
 
 // View engine setup
@@ -37,31 +60,30 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use('/node_modules', express.static(path.join(__dirname, '..', 'node_modules')));
 
-// Route setup
-app.use('/', landingRouter);
+// Routes
+let landingRouter = require('../routes/landing');
+let usersRouter = require('../routes/users');
+let publicRouter = require('../routes/public');
+let ticketRouter = require('../routes/ticket');
+let loginRouter = require('../routes/login');
+
+app.use('/',landingRouter);
 app.use('/users', usersRouter);
-app.use('/public', publicRouter); 
+app.use('/public', publicRouter);
 app.use('/ticket', ticketRouter);
-app.use('/login', loginRouter); // Ensure this is set up correctly
+app.use('/login', loginRouter);
 
 // 404 error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
-// Error handler
-// Error handler
-app.use(function(err, req, res, next) {
+// General error handler
+app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
-  // Set loggedIn to false if it's not defined
-  res.locals.loggedIn = req.query.loggedIn ? true : false;
-
   res.status(err.status || 500);
-  res.render('error', {
-      title: "Error"
-  });
+  res.render('error', { title: "Error" });
 });
 
 module.exports = app;
